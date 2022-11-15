38a55e3d3a2f17e8b63269a42edb29a89bc3c3cd
Kop <rolage@yandex.ru>
2022-11-15 06:16:46 +0300

    Client base

---
+++ server/server/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+
+namespace Alien
+{
+	public class Program
+	{
+		private static void Main(string[] args)
+		{
+			bool flag;
+			using (new Mutex(true, "rand-id-767223923742314124", out flag))
+			{
+				if (flag)
+				{
+					StateMachine stateMachine = new StateMachine();
+					Config.Init();
+					while (true)
+					{
+						try
+						{
+							Util.Log(string.Format("State : {0}", stateMachine.CurrentState));
+							switch (stateMachine.CurrentState)
+							{
+								case MachineState.Begin:
+									stateMachine.MoveNext();
+									break;
+								case MachineState.Sleep:
+									stateMachine.MoveNext();
+									break;
+								case MachineState.Alive:
+									stateMachine.MoveNext();
+									break;
+								case MachineState.Receive:
+									stateMachine.MoveNext();
+									break;
+								case MachineState.Do:
+									stateMachine.MoveNext();
+									break;
+								case MachineState.Send:
+									stateMachine.MoveNext();
+									break;
+								case MachineState.SendAndReceive:
+									stateMachine.MoveNext();
+									break;
+								case MachineState.SecondSleep:
+									stateMachine.MoveNext();
+									break;
+							}
+						}
+						catch (Exception ex)
+						{
+							Util.Log(ex.ToString());
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ server/server/Util.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace Alien
+{
+	internal class Util
+	{
+		public static void Log(string log)
+		{
+			Console.Write(
+				string.Concat(new string[]
+				{
+					"[ ",
+					DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
+					" ] ",
+					log,
+					Environment.NewLine
+				})
+			);
+		}
+	}
+}
