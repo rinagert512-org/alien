8d75a0668c37c20a8abb42bad41e9ddecbb60884
SergeevGeorge <sergeevgeorge@k733.edu>
2022-12-05 20:02:33 +0300

    __init__ for DnsServer is done

--- server/DNS_C2.py
+++ server/DNS_C2.py
@@ -1,11 +1,15 @@
 import logging
 from socketserver import BaseRequestHandler, ThreadingUDPServer
 
+from Alien import alien
+
 logger = logging.getLogger("DNS_C2")
 
 class DnsServer:
-    def __init__():
-        pass
+    def __init__(self, c2_server: alien, c2_domains: list[str]):
+        self.c2_server = c2_server
+        self.c2_domain = c2_domains
+        self.memory = {}
 
 class UdpRequestHandler(BaseRequestHandler):
     def __init__():
